8f90b498143ca574281b509d294328e3f62c9e7a
hardvlad <hardvlad@gmail.com>
2025-12-17 10:21:34 +0300

    Инкремент 5 - добавление переменных окружения

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 	"strings"
 )
 
@@ -15,7 +16,14 @@ func parseFlags() programFlags {
 	var flags programFlags
 
 	flag.StringVar(&flags.RunAddress, "a", ":8080", "адрес запуска HTTP-сервера")
+	if envRunAddr := os.Getenv("BASE_URL"); envRunAddr != "" {
+		flags.RunAddress = envRunAddr
+	}
+
 	flag.StringVar(&flags.ServerAddress, "b", "http://localhost:8080/", "базовый адрес результирующего сокращённого URL")
+	if envServAddr := os.Getenv("SERVER_ADDRESS"); envServAddr != "" {
+		flags.ServerAddress = envServAddr
+	}
 
 	if !strings.HasSuffix(flags.ServerAddress, "/") {
 		flags.ServerAddress += "/"
