521d403828c219c38c07cbe15b8b0d296013f3d0
hardvlad <hardvlad@gmail.com>
2025-12-17 13:02:49 +0300

    Инкремент 6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,14 +5,27 @@ import (
 
 	"github.com/hardvlad/ypshort/internal/config"
 	"github.com/hardvlad/ypshort/internal/handler"
+	"github.com/hardvlad/ypshort/internal/logger"
 	"github.com/hardvlad/ypshort/internal/repository"
 	"github.com/hardvlad/ypshort/internal/server"
 )
 
 func main() {
+
+	myLogger, err1 := logger.InitLogger()
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	defer myLogger.Sync()
+
 	flags := parseFlags()
-	err := server.StartServer(flags.RunAddress, handler.NewHandlers(config.NewConfig(flags.ServerAddress), repository.NewStorage()))
-	if err != nil {
-		log.Fatal(err)
+
+	logger.Sugar = *myLogger.Sugar()
+	logger.Sugar.Infow("Старт сервера", "addr", flags.RunAddress)
+
+	err2 := server.StartServer(flags.RunAddress, logger.WithLogging(handler.NewHandlers(config.NewConfig(flags.ServerAddress), repository.NewStorage())))
+	if err2 != nil {
+		logger.Sugar.Fatalw(err2.Error(), "event", "start server")
 	}
 }
