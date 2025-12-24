dc24fae8daa029eca7c2334020c03c81171cbb53
hardvlad <hardvlad@gmail.com>
2025-12-24 15:47:54 +0500

    Инкремент 11 - база данных и запросы в базу - фикс тестов

--- internal/repository/pg/pg.go
+++ internal/repository/pg/pg.go
@@ -11,25 +11,25 @@ import (
 )
 
 type Storage struct {
-	DbConn *sql.DB
+	DBConn *sql.DB
 	mu     sync.Mutex
 	logger *zap.SugaredLogger
 }
 
 func NewPGStorage(dbConn *sql.DB, logger *zap.SugaredLogger) Storage {
-	return Storage{DbConn: dbConn, logger: logger}
+	return Storage{DBConn: dbConn, logger: logger}
 }
 
 func (s Storage) Get(key string) (string, bool) {
-	row := s.DbConn.QueryRowContext(context.Background(), "SELECT url from saved_links where code = $1 limit 1", key)
+	row := s.DBConn.QueryRowContext(context.Background(), "SELECT url from saved_links where code = $1 limit 1", key)
 
-	var savedUrl string
-	err := row.Scan(&savedUrl)
+	var savedURL string
+	err := row.Scan(&savedURL)
 	if err != nil {
 		s.logger.Debugw(err.Error(), "event", "get from DB", key)
 		return "", false
 	}
-	return savedUrl, true
+	return savedURL, true
 }
 
 func (s Storage) Set(key, value string) error {
@@ -40,7 +40,7 @@ func (s Storage) Set(key, value string) error {
 		return fmt.Errorf("%w: %s", repository.ErrorKeyExists, key)
 	}
 
-	_, err := s.DbConn.ExecContext(context.Background(), "INSERT INTO saved_links (code, url) VALUES ($1, $2)", key, value)
+	_, err := s.DBConn.ExecContext(context.Background(), "INSERT INTO saved_links (code, url) VALUES ($1, $2)", key, value)
 	if err != nil {
 		return err
 	}
